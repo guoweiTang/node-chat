81fe77a3a01b56339060e3ac2c27837a12ec1c94
Ice <1214475704@qq.com>
2018-01-14 15:58:27 +0800

    add logger

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const favicon = require('serve-favicon');
-// const logger = require('morgan');
+const logger = require('morgan');
+const fileStreamRotator = require('file-stream-rotator');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 //基于cookie-parser的会话设置（https://github.com/expressjs/cookie-session）
 const cookieSession = require('cookie-session');
 const routes = require('./routes');
 
+const app = express();
+const NODE_ENV = process.env.NODE_ENV || 'production';
+
+/**
+ * 日志输出
+ */
+let logDir = path.join(__dirname, '.logs');
+if (NODE_ENV !== 'development') {
+  // 创建.logs目录
+  fs.existsSync(logDir) || fs.mkdirSync(logDir);
+  let accessLogStream = fileStreamRotator.getStream({
+      date_format: 'YYYYMMDD',
+      filename: path.join(logDir, 'access-%DATE%.log'),
+      frequency: 'daily',
+      verbose: true
+  });
+  app.use(logger('combined', {stream: accessLogStream}));
+}
+app.use(logger('dev'));
+  
+
 /**
  * 连接数据库
  */
 require('./db-model');
-
-const app = express();
 
 // 模板引擎
 app.set('views', path.join(__dirname, 'views'));
@@ -25,7 +46,6 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 /**
  * 一些组件
  */
-// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -58,8 +78,7 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = NODE_ENV === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
